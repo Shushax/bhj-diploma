3b8d6175f24061d8cb56ffcd1791bdc92dcb8b69
Shushax <nekfi@mail.ru>
2020-08-20 05:10:23 +0300

    createRequest первая версия

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,28 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    if (options.method === 'GET') {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', `${options.url}?mail=${options.data.mail}&password=${options.data.password}`);
+        xhr.withCredentials = true;
+        xhr.send();
+    } else {
+        let xhr = new XMLHttpRequest();
+        let formData = new FormData();
 
+        formData.append('mail', options.data.mail);
+        formData.append('password', options.data.password);
+        xhr.open('POST', options.url);
+        xhr.withCredentials = true;
+        xhr.send(formData);
+    }
+    // что делать с options.callback??
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            options.callback(err, xhr.response);
+        } else {
+            const error = new Error('Ошибка!')
+            options.callback(error, xhr.response);
+        }
+    }
 };
