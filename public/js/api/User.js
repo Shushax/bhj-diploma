3de6694b073450ec62408795addf00b995a36f62
Ksenia Sidorova <nekfi@mail.ru>
2020-08-23 02:19:55 +0300

    Добавлен User

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -4,12 +4,13 @@
  * Имеет свойство URL, равное '/user'.
  * */
 class User {
+  static URL = '/user';
   /**
    * Устанавливает текущего пользователя в
    * локальном хранилище.
    * */
   static setCurrent(user) {
-
+    localStorage.user = JSON.stringify(user);
   }
 
   /**
@@ -17,7 +18,7 @@ class User {
    * пользователе из локального хранилища.
    * */
   static unsetCurrent() {
-
+    localStorage.clear();
   }
 
   /**
@@ -25,7 +26,8 @@ class User {
    * из локального хранилища
    * */
   static current() {
-
+    let currentUser = JSON.parse(localStorage.user);
+    return currentUser;
   }
 
   /**
@@ -33,8 +35,20 @@ class User {
    * авторизованном пользователе.
    * */
   static fetch( data, callback = f => f ) {
-
-  }
+    createRequest(data);
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', `${options.url}/current`);
+      xhr.send();
+      xhr.onload = function() {
+        if (xhr.status == 200) {
+          if (xhr.response.success == true) {
+            User.setCurrent(xhr.response.user);
+          } else {
+            User.unsetCurrent(xhr.response.user);
+          }
+        }
+      }
+    }
 
   /**
    * Производит попытку авторизации.
@@ -42,8 +56,20 @@ class User {
    * сохранить пользователя через метод
    * User.setCurrent.
    * */
-  static login( data, callback = f => f ) {
-
+  static login (data, callback = f => f) {
+    createRequest(data);
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', `${options.url}/login`);
+    let formData = new FormData();
+    for (let option in data) {
+      formData.append(`${option}`, data[option]);
+    }
+    xhr.send(formData);
+    if (xhr.status == 200) {
+      if (xhr.response.success == true) {
+        User.setCurrent(xhr.response.user);
+      }
+    }
   }
 
   /**
@@ -53,14 +79,39 @@ class User {
    * User.setCurrent.
    * */
   static register( data, callback = f => f ) {
-
+    createRequest(data);
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', `${options.url}/register`);
+    let formData = new FormData();
+    for (let option in data) {
+      formData.append(`${option}`, data[option]);
+    }
+    xhr.send(formData);
+    if (xhr.status == 200) {
+      if (xhr.response.success == true) {
+       User.setCurrent(xhr.response.user);
+      }
+    }  
   }
+  
 
   /**
    * Производит выход из приложения. После успешного
    * выхода необходимо вызвать метод User.unsetCurrent
    * */
   static logout( data, callback = f => f ) {
-
+    createRequest(data);
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', `${options.url}/logout`);
+    let formData = new FormData();
+    for (let option in data) {
+      formData.append(`${option}`, data[option]);
+    }
+    xhr.send(formData);
+    if (xhr.status == 200) {
+      if (xhr.response.success == true) {
+       User.unsetCurrent();
+      }
+    }  
   }
 }
